ce4e8778f87020511dd1c9dfd21689aed040fb16
qzlu-cyber <941648981@qq.com>
2023-06-19 17:18:43 +0800

    抽象BufferLayout

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -11,6 +11,27 @@ namespace Hazel {
 
     Application* Application::s_Instance = nullptr;
 
+    static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+    {
+        switch (type)
+        {
+            case Hazel::ShaderDataType::Float:    return GL_FLOAT;
+            case Hazel::ShaderDataType::Float2:   return GL_FLOAT;
+            case Hazel::ShaderDataType::Float3:   return GL_FLOAT;
+            case Hazel::ShaderDataType::Float4:   return GL_FLOAT;
+            case Hazel::ShaderDataType::Mat3:     return GL_FLOAT;
+            case Hazel::ShaderDataType::Mat4:     return GL_FLOAT;
+            case Hazel::ShaderDataType::Int:      return GL_INT;
+            case Hazel::ShaderDataType::Int2:     return GL_INT;
+            case Hazel::ShaderDataType::Int3:     return GL_INT;
+            case Hazel::ShaderDataType::Int4:     return GL_INT;
+            case Hazel::ShaderDataType::Bool:     return GL_BOOL;
+        }
+
+        HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+        return 0;
+    }
+
     Application::Application()
     {
         HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -23,14 +44,14 @@ namespace Hazel {
         PushOverlay(m_ImGuiLayer);
 
         float vertices[] = {
-            -0.5f, -0.5f, 0.0f,
-             0.5f, -0.5f, 0.0f,
-             0.0f,  0.5f, 0.0f
+            -0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+             0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+             0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
         };
 
         uint32_t indices[] = { 0, 1, 2 };
 
-        // OpenGL 核心模式强制使用 VAO (顶点数组对象) 
+        // OpenGL 核心模式强制使用 VAO (顶点数组对象)
         glGenVertexArrays(1, &m_VertexArray);
         glBindVertexArray(m_VertexArray);
 
@@ -40,53 +61,83 @@ namespace Hazel {
         // 将索引数组到一个索引缓冲中
         m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 
-        // 设定顶点属性指针
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-        glEnableVertexAttribArray(0);
+        {
+            BufferLayout layout = {
+                { ShaderDataType::Float3, "a_Position" },
+                { ShaderDataType::Float4, "a_Color" }
+            };
+
+            m_VertexBuffer->SetLayout(layout);
+        }
+
+        uint32_t index = 0;
+        const BufferLayout& layout = m_VertexBuffer->GetLayout();
+        for (const auto& element : layout) {
+            // 设定顶点属性指针
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(
+                index, 
+                element.GetComponentCount(), 
+                ShaderDataTypeToOpenGLBaseType(element.Type), 
+                element.Normalized ? GL_TRUE : GL_FALSE, 
+                layout.GetStride(), 
+                (const void*)(element.Offset)
+            );
+            index++;
+        }
 
         std::string vertexSrc = R"(
 			#version 330 core
-			
+
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
 			out vec3 v_Position;
+            out vec4 v_Color;
+
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = vec4(a_Position, 1.0);	
+                v_Color = a_Color;
+				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
 
         std::string fragmentSrc = R"(
 			#version 330 core
-			
+
 			layout(location = 0) out vec4 color;
+
 			in vec3 v_Position;
+            in vec4 v_Color;
+
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+                color = v_Color;
 			}
 		)";
 
         m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
     }
 
-    Application::~Application() 
+    Application::~Application()
     {
     }
 
-    void Application::PushLayer(Layer* layer) 
+    void Application::PushLayer(Layer* layer)
     {
         m_LayerStack.PushLayer(layer);
         layer->OnAttach();
     }
 
-    void Application::PushOverlay(Layer* overlay) 
+    void Application::PushOverlay(Layer* overlay)
     {
         m_LayerStack.PushOverlay(overlay);
         overlay->OnAttach();
     }
 
-    void Application::OnEvent(Event& e) 
+    void Application::OnEvent(Event& e)
     {
         EventDispatcher dispatcher(e);
         dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose)); // 检查事件 e 的 EventType 是否是 WindowClose，如果是就执行 OnWindowClose 函数
@@ -101,7 +152,7 @@ namespace Hazel {
         }
     }
 
-    void Application::Run() 
+    void Application::Run()
     {
         while (m_Running) {
             glClearColor(0.2f, 0.2f, 0.2f, 1);
@@ -123,7 +174,7 @@ namespace Hazel {
         }
     }
 
-    bool Application::OnWindowClose(WindowCloseEvent& e) 
+    bool Application::OnWindowClose(WindowCloseEvent& e)
     {
         m_Running = false;
         return true;
